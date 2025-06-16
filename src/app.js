b63b4715b0661d9eee72bf196db01f152150f476
Vineethkumar1998 <vineethkumar1607@gmail.com>
2025-06-16 23:21:08 +0530

    order of routes matters

--- src/app.js
+++ src/app.js
@@ -1,30 +1,24 @@
 const express = require('express');
 const app = express();
 
-// A Route can have many request handlers, like below first argumment is route and second and third aruguments are first and second route handlers respectively
-app.get("/users", [(req, res, next) => {
+// this a route and middleware function
+app.get("/users", (req, res, next) => {
     console.log("handling the route handler 1");
-
-    next()
-    res.send("respose 1 ")
-
-}, (req, res, next) => {
-    console.log("handling the route handler 2")
-    // res.send(" response 2")\
-    next()
-}], (req, res, next) => {
-    console.log("handling the route handler 3")
-    // res.send(" response 3")
-    next();
-}, (req, res, next) => {
-    console.log("handling the route handler 4")
-    // res.send(" response 4")
-    next()
-}, (req, res, next) => {
-    console.log("handling the route handler 5")
-    // res.send(" response 5")
+    // res.send("respose 1 ")
     next()
 })
+
+// this is another route  and middleware function
+app.get("/users", (req, res, next) => {
+    console.log("handling the route handler 2");
+    res.send("respose 2 ")
+})
+
+app.use("/", (req, res, next) => {
+    res.send("from app.use / route")
+    next()
+})
+
 
 app.listen(7777, () => {
     console.log("server is listening to the port 7777")
